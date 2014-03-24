31ceae495563a3f121dbd0e01a60e39e35f06d6b
Jeremy Crawford <jcrawford.design@gmail.com>
2014-03-24 01:27:50 -0400

    added jquery scrolls with new button and changed styles of the icon links

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,25 @@
 console.log('\'Allo \'Allo!');
+
+$(".work-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".work").offset().top
+    }, 1000);
+});
+
+$(".about-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".about").offset().top
+    }, 1000);
+});
+
+$(".contact-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".footer").offset().top
+    }, 1000);
+});
+
+$(".scroll-up-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".main").offset().top
+    }, 2000);
+});
