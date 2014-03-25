bbc83a2e66ca83c0417ab2cf312231836680f91b
Jeremy Crawford <jcrawford.design@gmail.com>
2014-03-25 02:24:52 -0400

    added email icon and link and added webkit transitions

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,25 +1,25 @@
 console.log('\'Allo \'Allo!');
 
-$(".work-btn").click(function() {
+$("#work-btn").click(function() {
     $('html, body').animate({
-        scrollTop: $(".work").offset().top
+        scrollTop: $("#work").offset().top
+    }, 1250);
+});
+
+$("#about-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
     }, 1000);
 });
 
-$(".about-btn").click(function() {
+$("#contact-btn").click(function() {
     $('html, body').animate({
-        scrollTop: $(".about").offset().top
-    }, 1000);
+        scrollTop: $("#footer").offset().top
+    }, 750);
 });
 
-$(".contact-btn").click(function() {
+$("#scroll-up-btn").click(function() {
     $('html, body').animate({
-        scrollTop: $(".footer").offset().top
-    }, 1000);
-});
-
-$(".scroll-up-btn").click(function() {
-    $('html, body').animate({
-        scrollTop: $(".main").offset().top
-    }, 2000);
+        scrollTop: $("#main").offset().top
+    }, 500);
 });
