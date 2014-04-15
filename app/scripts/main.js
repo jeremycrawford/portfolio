f55e938369b49da3ca25158ade92fcad85583d71
Jeremy Crawford <jcrawford.design@gmail.com>
2014-04-15 18:57:22 -0400

    added header scroll hidden

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -22,3 +22,20 @@ $("#scroll-up-btn").click(function() {
     $('body').animate({
         scrollTop: 0}, 500);
 });
+
+// $(window).scroll(function () { 
+//   if ($('body').scrollTop() < 380) {
+//     $("#logo-page-header").removeClass("logo-page-header-hidden", 1500);
+//   }
+//       else
+//         $("#logo-page-header").addClass("logo-page-header-hidden", 1000);
+// });
+
+$(window).scroll(function () { 
+  if ($(this).scrollTop() < 300) {
+    $('#logo-page-header').fadeIn(500);
+  } else {
+    // $('#logo-page-header').hide();
+    $("#logo-page-header").fadeOut(500);
+  }
+});
